4fc55373a4689ce353465e7b04c4def53cc9c3db
mysteryman386 <65476730+mysteryman386@users.noreply.github.com>
2025-04-28 22:27:07 +1200

    Update KerbalSprigProgram.ino

--- KerbalSprigProgram.ino
+++ KerbalSprigProgram.ino
@@ -1,6 +1,17 @@
+/*
+    Kerbal Sprig Program V3
+    KerbalSprigProgram.ino
+
+    This program is an alternate firmware for the Sprig that interfaces with Kerbal Space Program via the Kerbal Simpit mod.
+
+    This Version Modified 28/04/2025
+    By Wilmer Zhang
+
+*/
+
 #include <Arduino.h>
-#include <Adafruit_GFX.h>     // Core graphics library
-#include <Adafruit_ST7735.h>  // Hardware-specific library for ST7735
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
 #include <SPI.h>
 #include <string.h>
 #include "KerbalSimpit.h"
@@ -9,21 +20,21 @@
 #include "screenHandler.h"
 
 //SPRIG PIN DEFINITIONS, DO NOT TOUCH UNLESS YOU ARE AN EXPERT
-#define R_LED 4
-#define L_LED 28
-#define KEY_W 5
-#define KEY_A 6
-#define KEY_S 7
-#define KEY_D 8
-#define KEY_I 12
-#define KEY_J 13
-#define KEY_K 14
-#define KEY_L 15
+constexpr int R_LED = 4;
+constexpr int L_LED = 28;
+constexpr int KEY_W = 5;
+constexpr int KEY_A = 6;
+constexpr int KEY_S = 7;
+constexpr int KEY_D = 8;
+constexpr int KEY_I = 12;
+constexpr int KEY_J = 13;
+constexpr int KEY_K = 14;
+constexpr int KEY_L = 15;
 //USER DEFINED SETTINGS
-#define armAbortAbove 2000       //When the craft is above this altitude in Meters, the auto abort system is armed
-#define triggerAbortBelow 1750   //When the craft is below this altitude, and satisfying speed requirements, the auto abort system is triggered
-#define triggerAbortSpeed 75     //When the craft is faster than this vertical speed (m/s), and when below TAB altitude, the auto abort system is triggered
-#define screenSyncInterval 1000  //How much time elapses (ms) before the screen updates with new data
+constexpr int armAbortAbove = 2000;       //When the craft is above this altitude in Meters, the auto abort system is armed
+constexpr int triggerAbortBelow = 1750;   //When the craft is below this altitude, and satisfying speed requirements, the auto abort system is triggered
+constexpr int triggerAbortSpeed = 75;     //When the craft is faster than this vertical speed (m/s), and when below TAB altitude, the auto abort system is triggered
+constexpr int screenSyncInterval = 2500;  //How much time elapses (ms) before the screen updates with new data
 
 int keyState[] = { 0, 0, 0, 0, 0, 0, 0, 0 };  //WASD, IJKL
 cameraRotationMessage camMsg;
@@ -36,13 +47,61 @@ int lastPitch = 0;
 KerbalSimpit mySimpit(Serial);
 screenHandler sprigScreen;
 
+// Create ButtonHandler objects for each button
+ButtonHandler buttons[] = {
+  ButtonHandler(KEY_W),
+  ButtonHandler(KEY_A),
+  ButtonHandler(KEY_S),
+  ButtonHandler(KEY_D),
+  ButtonHandler(KEY_I),
+  ButtonHandler(KEY_J),
+  ButtonHandler(KEY_K),
+  ButtonHandler(KEY_L)
+};
+
+void setup() {  //initial setup, this piece of code runs when the sprig starts.
+  Serial.begin(115200);
+  pinMode(R_LED, OUTPUT);
+  pinMode(L_LED, OUTPUT);
+  for (int i = 4; i < 8; i++) {  //Sets the debounce state of the right hand buttons to false
+    buttons[i].setDebounceState(false);
+  }
+  rp2040.idleOtherCore();
+
+  sprigScreen.startScreen();  //Sprig screen starts
+  while (!mySimpit.init()) {
+    digitalWrite(R_LED, HIGH);
+    tft.print(".");
+    delay(100);
+    digitalWrite(R_LED, LOW);
+  }
+  digitalWrite(L_LED, HIGH);
+  mySimpit.printToKSP("Kerbal Sprig Program has connected.", PRINT_TO_SCREEN);
+  mySimpit.inboundHandler(messageHandler);  //These lines allow the sprig to interact with KerbalSimpit, allowing two way communication of information.
+  mySimpit.registerChannel(ALTITUDE_MESSAGE);
+  mySimpit.registerChannel(SCENE_CHANGE_MESSAGE);
+  mySimpit.registerChannel(VELOCITY_MESSAGE);
+  mySimpit.registerChannel(APSIDES_MESSAGE);
+  rp2040.resumeOtherCore();
+}
+
+void setup1() {
+}
+
+void loop() {
+  handleKeyPresses();
+  mySimpit.update();
+}
+
+void loop1() {
+  sprigScreen.syncScreen(screenSyncInterval, secondControls);
+}
 
 void abortAction() {  //This function deactivates SAS, and activates the abort action group.
+  rp2040.idleOtherCore();
   mySimpit.deactivateAction(SAS_ACTION);
   mySimpit.activateAction(ABORT_ACTION);
-  tft.fillScreen(ST77XX_RED);
-  tft.setCursor(0, 0);
-  tft.println("Aborting Vessel:");
+  sprigScreen.errorScreen("Aborting Vessel:");
   autoAbort = false;
   for (int i = 0; i <= 5; i++) {  // loop that flashes the right LED on and off for 1 second
     digitalWrite(R_LED, HIGH);
@@ -50,6 +109,7 @@ void abortAction() {  //This function deactivates SAS, and activates the abort a
     digitalWrite(R_LED, LOW);
     delay(100);
   }
+  rp2040.resumeOtherCore();
 }
 
 void actionBind(uint8_t ActionGroup, String message) {  //This function replaces lightAction(), sasAction(), and stageAction() from V1, putting them all into one function that rules them all. ActionGroup is for the actiongroup needed to be set, and message is for the screen popup
@@ -57,16 +117,12 @@ void actionBind(uint8_t ActionGroup, String message) {  //This function replaces
   sprigScreen.showStats(message, secondControls);
 }
 
-void camYaw(int yawFactor) {  //This function rotates the camera in a yaw motion (+ve yawFactor moves the camera right, -ve yawFactor moves the camera left)
+void camMovement(int pitchFactor, int yawFactor) {  //This function rotates the camera in a pitch motion (+ve pitchFactor moves the camera up, -ve pitchFactor moves the camera down) and yaw motion (+ve yawFactor moves the camera right, -ve yawFactor moves the camera left)
+  camMsg.setPitch(pitchFactor);
   camMsg.setYaw(yawFactor);
   mySimpit.send(CAMERA_ROTATION_MESSAGE, camMsg);
-  camMsg.setYaw(0);
-}
-
-void camPitch(int pitchFactor) {  //This function rotates the camera in a pitch motion (+ve pitchFactor moves the camera up, -ve pitchFactor moves the camera down)
-  camMsg.setPitch(pitchFactor);
-  mySimpit.send(CAMERA_ROTATION_MESSAGE, camMsg);
   camMsg.setPitch(0);
+  camMsg.setYaw(0);
 }
 
 void thrustAction(int control) {  //This function controls thrust control (0-32767, 0 being no thrust, 32767 being full thrust)
@@ -86,43 +142,30 @@ void controlYawPitch(int yawFactor, int pitchFactor) {  //This function controls
   delay(75);
 }
 
-
-// Create ButtonHandler objects for each button
-ButtonHandler buttons[] = {
-  ButtonHandler(KEY_W),
-  ButtonHandler(KEY_A),
-  ButtonHandler(KEY_S),
-  ButtonHandler(KEY_D),
-  ButtonHandler(KEY_I),
-  ButtonHandler(KEY_J),
-  ButtonHandler(KEY_K),
-  ButtonHandler(KEY_L)
-};
-
 // Function pointer type for button actions
 typedef void (*ButtonAction)();
 void (*primaryActions[])() = {  //Actions binded to the primary action section
-  []() {//W
+  []() {                        //W
     actionBind(STAGE_ACTION, "Staged!");
   },
-  []() {//A
+  []() {  //A
     actionBind(SAS_ACTION, "SAS toggled!");
   },
-  abortAction,//S
-  []() {//D
+  abortAction,  //S
+  []() {        //D
     actionBind(LIGHT_ACTION, "Lights toggled!");
   },
-  []() {           
-    camPitch(10);  //I
+  []() {
+    camMovement(10, 0);  //I
   },
   []() {
-    camYaw(10);  //J
+    camMovement(0, 10);  //J
   },
   []() {
-    camPitch(-10);  //K
+    camMovement(-10, 0);  //K
   },
   []() {
-    camYaw(-10);  //L
+    camMovement(0, -10);  //L
   }
 };
 void (*secondaryActions[])() = {
@@ -151,50 +194,20 @@ void (*secondaryActions[])() = {
   }  // L
 };
 
-void setup() {  //initial setup, this piece of code runs when the sprig starts.
-  Serial.begin(115200);
-  pinMode(R_LED, OUTPUT);
-  pinMode(L_LED, OUTPUT);
-  for (int i = 4; i < 8; i++) {  //Sets the debounce state of the right hand buttons to false
-    buttons[i].setDebounceState(false);
-  }
-  sprigScreen.startScreen();  //Sprig screen starts
-  tft.println("Kerbal Sprig Program activated\n");
-  tft.println("Handshaking.");
-  while (!mySimpit.init()) {
-    digitalWrite(R_LED, HIGH);
-    tft.print(".");
-    delay(100);
-    digitalWrite(R_LED, LOW);
-  }
-  digitalWrite(L_LED, HIGH);
-  mySimpit.printToKSP("Kerbal Sprig Program has connected.", PRINT_TO_SCREEN);
-  mySimpit.inboundHandler(messageHandler);  //These lines allow the sprig to interact with KerbalSimpit, allowing two way communication of information.
-  mySimpit.registerChannel(ALTITUDE_MESSAGE);
-  mySimpit.registerChannel(SCENE_CHANGE_MESSAGE);
-  mySimpit.registerChannel(VELOCITY_MESSAGE);
-  mySimpit.registerChannel(APSIDES_MESSAGE);
-}
-
-void loop() {
-  handleKeyPresses();
-  mySimpit.update();
-  sprigScreen.syncScreen(screenSyncInterval, secondControls);
-}
-
 void messageHandler(byte messageType, byte msg[], byte msgSize) {  //This function is called with mySimpit.update(), updating information.
   switch (messageType) {
-    case APSIDES_MESSAGE:
+    case APSIDES_MESSAGE:  // small function that grabs orbital parameters, and passes them on to the screenhandler
       if (msgSize == sizeof(apsidesMessage)) {
         apsidesMessage myApsides;
         myApsides = parseMessage<apsidesMessage>(msg);
         sprigScreen.updateFloatInfo(2, myApsides.apoapsis);
         sprigScreen.updateFloatInfo(3, myApsides.periapsis);
       }
+      break;
     case SCENE_CHANGE_MESSAGE:
       if (msgSize == sizeof(byte)) {
-        byte scene = msg[0];  // Extract scene info
-        switch (scene) {      //This sets the autoabort flag to false, so abortion sequence does not happen when you revert flight or something.
+        byte scene = msg[0];
+        switch (scene) {  //This sets the autoabort flag to false, so abortion sequence does not happen when you revert flight or something.
           case 0:
           case 1:
           case 2:
@@ -204,7 +217,7 @@ void messageHandler(byte messageType, byte msg[], byte msgSize) {  //This functi
         }
       }
       break;
-    case VELOCITY_MESSAGE:  //This function grabs vertical velocity and stores it in screenhandler.h
+    case VELOCITY_MESSAGE:  //This case grabs vertical velocity and stores it in screenhandler.h
       if (msgSize == sizeof(velocityMessage)) {
         velocityMessage myVelocity = parseMessage<velocityMessage>(msg);
         if (sprigScreen.getFloatInfo(0) != myVelocity.vertical) {
@@ -212,7 +225,7 @@ void messageHandler(byte messageType, byte msg[], byte msgSize) {  //This functi
         }
       }
       break;
-    case ALTITUDE_MESSAGE:  //Bigger function that handles the autoabort logic. It checks if certain criteria has been set, and activates abortion sequence if it does.
+    case ALTITUDE_MESSAGE:  //Bigger case that grabs altitude data and handles the autoabort logic. It checks if certain criteria has been set, and activates abortion sequence if it does.
       if (msgSize == sizeof(altitudeMessage)) {
         altitudeMessage myAltitude;
         myAltitude = parseMessage<altitudeMessage>(msg);
@@ -245,7 +258,7 @@ void handleKeyPresses() {  //This function handles key presses, and calls the ap
       secondControls = false;
     }
   }
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++) {  //Rolls through buttons, and checks if any are pressed. If so, excute their specific functions
     if (buttons[i].isPressed()) {
       if (secondControls && secondaryActions[i] != nullptr) {
         secondaryActions[i]();
