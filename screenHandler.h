afcad213c8fd3e37200c1b24a86fd251499ba1c9
mysteryman386 <65476730+mysteryman386@users.noreply.github.com>
2025-04-28 22:27:38 +1200

    Update screenHandler.h

--- screenHandler.h
+++ screenHandler.h
@@ -1,15 +1,26 @@
+/*
+    Kerbal Sprig Program V3
+    screenHandler.h
+
+    This header file contains code for the screen handler, all encapsulated in a neat class
+
+    This Version Created 28/04/2025
+    By Wilmer Zhang
+
+*/
+
 #include <Arduino.h>
-#include <Adafruit_GFX.h>     // Core graphics library
-#include <Adafruit_ST7735.h>  // Hardware-specific library for ST7735
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
 /////////////////PINS SPECIFIC TO THE SPRIG, DO NOT TOUCH UNLESS YOU ARE ADAPTING THIS TO ANOTHER DEVICE
-#define TFT_CS 20
-#define TFT_RST 26
-#define TFT_DC 22
-#define TFT_BACKLIGHT 17
+constexpr int TFT_CS = 20;
+constexpr int TFT_RST = 26;
+constexpr int TFT_DC = 22;
+constexpr int TFT_BACKLIGHT = 17;
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class screenHandler { //Screenhandler class. These functions handle the higher level aspects of manipulating the screen.
+class screenHandler {  //Screenhandler class. These functions handle the higher level aspects of manipulating the screen.
 private:
   bool backlightState = false;
   bool drawState = false;
@@ -50,11 +61,11 @@ public:
         return vesselPeriapsis;
         break;
     }
-    return NULL;
+    return -1;
   }
 
-  void startScreen() { //This function handles the startup function of the tft screen.
-    pinMode(TFT_BACKLIGHT, OUTPUT); 
+  void startScreen() {  //This function handles the startup function of the tft screen.
+    pinMode(TFT_BACKLIGHT, OUTPUT);
     tft.initR(INITR_BLACKTAB);
     digitalWrite(TFT_BACKLIGHT, HIGH);
     backlightState = true;
@@ -63,8 +74,10 @@ public:
     tft.setCursor(0, 0);
     tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
     tft.setTextWrap(true);
+    tft.println("Kerbal Sprig Program activated\n");
+    tft.println("Handshaking.");
   }
-  void toggleBacklight() { //When called, this function either disables or enables the backlight, depending on current backlight status.
+  void toggleBacklight() {  //When called, this function either disables or enables the backlight, depending on current backlight status.
     if (backlightState == false) {
       digitalWrite(TFT_BACKLIGHT, HIGH);
       backlightState = true;
@@ -73,7 +86,8 @@ public:
       backlightState = false;
     }
   }
-  void showStats(String message, bool secondControls) { //This function wipes the screen, and shows statistics on the screen. It also shows custom messages that may be handy to the end user, using the Message variable.
+  void showStats(String message, bool secondControls) {  //This function wipes the screen, and shows statistics on the screen. It also shows custom messages that may be handy to the end user, using the Message variable.
+    rp2040.idleOtherCore();
     tft.fillScreen(ST77XX_BLACK);
     tft.setCursor(0, 0);
     tft.println(message);
@@ -89,14 +103,25 @@ public:
       tft.print(vesselPeriapsis);
     }
     tft.println("m Periapsis");
-    tft.print("Second Controls = ");
-    tft.println(secondControls);
+    tft.print("Second Controls ");
+    if (secondControls == true) {
+      tft.println("Enabled");
+    } else {
+      tft.println("Disabled");
+    }
+    rp2040.resumeOtherCore();
   }
-  void syncScreen(long syncInterval, bool secondControls) { //This function calls the showStats() function every x milliseconds. This is determined by the syncInterval() variable.
+  void syncScreen(long syncInterval, bool secondControls) {  //This function calls the showStats() function every x milliseconds. This is determined by the syncInterval() variable.
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= syncInterval) {
       showStats(" ", secondControls);
       previousMillis = currentMillis;
     }
+  }
+
+  void errorScreen(String message) {  //Neat little function for a big red screen, for errors. Perfect for the autoAbort function
+    tft.fillScreen(ST77XX_RED);
+    tft.setCursor(0, 0);
+    tft.println(message);
   }
 };
